36361caceb8de0e89b2af301032b48634b350e7c
PardeepKumar <pk.raswant@gmail.com>
2017-12-02 16:43:04 +0530

    Update signin.js - Codepanel

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,26 @@
-var newRegister=function(req,res,next){
 
+var signin=require('.././controllers/signin');
+
+var newRegister=function(req,res,next){
+    var emp_id=req.body.emp_id;
+    var name=req.body.name;
+    var password=req.body.password;
+    
+    var data={
+      emp_id:emp_id,
+      name:name,
+      password:password
+    };
+    
+    signin.register(data,function(err,result){
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send(result);
+        }
+    });
+    
     
 }
 
