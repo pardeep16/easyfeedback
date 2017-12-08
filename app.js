82fc32c96fa2280b604851785b41b53ac9d59eeb
pardeep <pk.raswant@gmail.com>
2017-12-08 08:37:42 +0530

    uppdate

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/app/release1/android',express.static(path.join('./upload/')));
 app.use('/techquiz',express.static(path.join('./upload/learnersweekfinal.apk')));
-app.use('/techquiz/d',express.static(path.join('./upload/learnersweekfinal.apk')));
-app.use('/techquiz/a',express.static(path.join('./upload/learnersweekdigital.apk')));
+//app.use('/techquiz/d',express.static(path.join('./upload/learnersweekfinal.apk')));
+app.use('/techquiz/app',express.static(path.join('./upload/learnersweekdigital.apk')));
 
 app.use('/', routes);
 app.use('/users', users);
